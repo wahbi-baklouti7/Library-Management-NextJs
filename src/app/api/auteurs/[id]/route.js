72f86c947c62ff159fe4840571618b0e7c97a7e2
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2024-05-16 09:54:39 +0100

    add update and delete auteur functionality

--- src/app/api/auteurs/[id]/route.js
+++ src/app/api/auteurs/[id]/route.js
@@ -3,19 +3,74 @@ import Auteur from "@/models/Auteur";
 import { HttpStatusCode } from "axios";
 import { NextResponse } from "next/server";
 
-
-
 export async function GET(_, { params }) {
-    try {
+  try {
     await connectDB();
     const auteur = await Auteur.findById(params.id);
     if (auteur) {
-    return NextResponse.json({ auteur });
+      return NextResponse.json({ auteur });
     }
-    return NextResponse.json({ message: `Auteur ${params.id} not found` }, {
-    status: HttpStatusCode.NotFound });
-    } catch (error) {
-    return NextResponse.json({ message: error }, { status:
-    HttpStatusCode.BadRequest });
+    return NextResponse.json(
+      { message: `Auteur ${params.id} not found` },
+      {
+        status: HttpStatusCode.NotFound,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error },
+      { status: HttpStatusCode.BadRequest }
+    );
+  }
+}
+
+export async function PUT(req, { params }) {
+  try {
+    await connectDB();
+    const auteur = await Auteur.findById(params.id);
+    if (auteur) {
+      const body = await req.json();
+      auteur.nomauteur = body.nomauteur;
+      auteur.email = body.email;
+      auteur.numtel = body.numtel;
+      auteur.save();
+      return NextResponse.json({ auteur });
     }
+    return NextResponse.json(
+      { message: `Auteur ${params.id} not found` },
+      { status: HttpStatusCode.NotFound }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error },
+      { status: HttpStatusCode.BadRequest }
+    );
+  }
+}
+
+export async function DELETE(_, { params }) {
+  try {
+    await connectDB();
+    const auteur = await Auteur.findById(params.id);
+    if (auteur) {
+      await Auteur.findByIdAndDelete(auteur._id);
+      return NextResponse.json({
+        message: `Auteur ${params.id} has been
+        
+        deleted`,
+      });
     }
+
+    return NextResponse.json(
+      { message: `Auteur ${params.id} not found` },
+      {
+        status: HttpStatusCode.NotFound,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: error },
+      { status: HttpStatusCode.BadRequest }
+    );
+  }
+}
